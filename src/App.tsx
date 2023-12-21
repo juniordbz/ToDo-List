d205c9023756002aa27d8c286cf399d30b5f6bd4
Francisco Bezerra da Silva Junior <franciscojunior02@hotmail.com.br>
2023-12-21 15:18:43 -0300

    Adicionar tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Header } from "./components/Header"
+import { Task } from "./components/addTask"
 
 function App() {
   return (
     <div>
       <Header/>
+      <Task/>
     </div>
   )
 }
+
 
 export default App
---
+++ src/components/addTask.tsx
@@ -0,0 +1,21 @@
+import styles from './addTask.module.css';
+import { PlusCircle } from 'lucide-react';
+
+
+export function Task(){
+  return(
+    <div>
+      <form className={styles.form}>
+        <textarea placeholder='Adicione uma nova tarefa'/>
+        <button 
+        className={styles.btn}
+        type='submit'>
+          <p>Criar</p>
+          <PlusCircle
+          size={15}
+          />
+        </button>
+      </form>
+    </div>
+  )
+}
