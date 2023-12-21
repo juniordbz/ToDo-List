54abb032598ff52e043df267d42de831a4aca8bb
Francisco Bezerra da Silva Junior <franciscojunior02@hotmail.com.br>
2023-12-21 13:35:38 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { Header } from "./components/Header"
+
 function App() {
   return (
-    <p>Hello World</p>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import header from '../assets/header.svg'
+import styles from './Header.module.css';
+
+export function Header(){
+  return(
+    <div className={styles.header}>
+      <img src={header} alt="logo" />
+    </div>
+
+  )
+}
