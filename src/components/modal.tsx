b74b166a3db0970968b745631ba8888e8fd33b0d
juniordbz <franciscojunior02@hotmail.com.br>
2024-05-17 20:08:23 -0300

    styles modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,3 +1,4 @@
+import { X } from 'lucide-react'
 import styles from './modal.module.css'
 import * as Dialog from '@radix-ui/react-dialog'
 
@@ -11,18 +12,27 @@ export function Modal({ setOpen, deleteTask }: ModalProps) {
     <Dialog.DialogPortal>
       <Dialog.Overlay className={styles.overlay} />
       <Dialog.Content className={styles.content}>
-        <Dialog.Close>X</Dialog.Close>
-        <span>Tem certeza que deseja limpar toda a lista de tarefas?</span>
+        <Dialog.Close className={styles.close}>
+          <X size={20} />
+        </Dialog.Close>
+        <span className={styles.text}>
+          Tem certeza que deseja limpar toda a lista de tarefas?
+        </span>
 
-        <button
-          onClick={() => {
-            deleteTask()
-            setOpen(false)
-          }}
-        >
-          Confirmar
-        </button>
-        <button onClick={() => setOpen(false)}>Cancelar</button>
+        <div className={styles.btnContainer}>
+          <button
+            className={styles.btnConfirmar}
+            onClick={() => {
+              deleteTask()
+              setOpen(false)
+            }}
+          >
+            Confirmar
+          </button>
+          <button className={styles.btnCancel} onClick={() => setOpen(false)}>
+            Cancelar
+          </button>
+        </div>
       </Dialog.Content>
     </Dialog.DialogPortal>
   )
